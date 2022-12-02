801eaebea19d7d237d20836c322e3c4cf5e5f88f
chand <chand@MSI>
2022-12-02 23:01:57 +0530

    commiting

---
+++ src/main/java/sonar533/Sonar533/App.java
@@ -0,0 +1,13 @@
+package sonar533.Sonar533;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
